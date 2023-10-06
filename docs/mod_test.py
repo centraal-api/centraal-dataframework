890dbd0b14a7b6df655b2ead06391846fcee3a72
German <german@centraal.studio>
2023-10-06 16:49:54 -0500

    poc

--- docs/mod_test.py
+++ docs/mod_test.py
@@ -7,3 +7,9 @@ from centraal_dataframework.tasks import task
 def greetins():
     """Saluda."""
     print("hello there, iam a task")
+
+
+@task
+def hello():
+    """Saluda."""
+    print("hola, que mas pues!, soy una tarea")
