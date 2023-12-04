3f752f772b5871b6c5171d45b16465b0aedcb396
German <german@centraal.studio>
2023-12-04 13:47:36 -0500

    WIP: runner

--- centraal_dataframework/blueprints.py
+++ centraal_dataframework/blueprints.py
@@ -1,5 +1,7 @@
 """Modulo de blueprints."""
+import os
 import logging
+import datetime
 
 import azure.functions as func
 from centraal_dataframework.runner import Runner
@@ -12,27 +14,44 @@ from centraal_dataframework.email_sender import send_email_error
 
 logger = logging.getLogger(__name__)
 NAME_CONNECTION_STORAGE_ACCOUNT = "MyStorageAccountAppSetting"
-QUEUE_NAME = "tareas"
+QUEUE_NAME = os.environ.get("QUEUE_NAME", "tareas")
 runner = Runner()
 framework = func.Blueprint()
+
+
+@framework.schedule(schedule="0 0 * * * *", arg_name="mytimer", run_on_startup=False)
+@framework.queue_output(arg_name="msg", queue_name=QUEUE_NAME, connection=NAME_CONNECTION_STORAGE_ACCOUNT)
+def check_and_schedule_task(mytimer: func.TimerRequest, msg: func.Out[str]) -> func.HttpResponse:
+    """Verifica que tareas se deben programar.
+
+    Args:
+        req: request de la funcion, debe tener el parametro llamado "task_name",
+            para ejecutar una lista de funciones en especifico.
+        msg: mensaje que representa la cola en donde adicionar.
+    """
+    utc_timestamp = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
+
+    if mytimer.past_due:
+        logging.warning('La función se ejecuto a fuetra de tiempo')
+
+    tareas = runner.get_tareas_programables(utc_timestamp)
+    if tareas is None:
+        logger.warning("No se programaron tareas")
+    else:
+        logging.info("se programan %s", tareas)
+        msg.set(tareas)
 
 
 @framework.route(methods=['post'])
 @framework.queue_output(arg_name="msg", queue_name=QUEUE_NAME, connection=NAME_CONNECTION_STORAGE_ACCOUNT)
 def queue_task(req: func.HttpRequest, msg: func.Out[str]) -> func.HttpResponse:
-    """Lee el archivo yml y define que tareas ejecutar.
-
-    Tambien puede usar para programar una lista de funciones.
+    """Adiciona una tarea a la cola de progrmacion.
 
     Args:
-        req: request de la funcion, puede tener un body vacio o tener un parametro
-            "task_name", para ejecutar una lista de funciones en especifico.
-        msg: mensaje que representa
-
+        req: request de la funcion, debe tener el parametro llamado "task_name",
+            para ejecutar una lista de funciones en especifico.
+        msg: mensaje que representa la cola en donde adicionar.
     """
-    logger.info('ejecutando la funcion run_tasks')
-    logger.info('leyendo el archivo .yaml de configuración')
-
     try:
         req_body = req.get_json()
         task_names = req_body.get('task_name', [])
@@ -43,15 +62,11 @@ def queue_task(req: func.HttpRequest, msg: func.Out[str]) -> func.HttpResponse:
             status_code=400,
         )
 
-    if task_names is not None:
+    if task_names is None:
+        return func.HttpResponse("No se enviaron tareas", status_code=400)
         # se ejecuta tarea con el nombre especifico sin importar el archivo de configuracion
-        msg.set(task_names)
-    else:
-        # de acuerdo al retorno del archivo yml, solo se encolan el listado
-
-        pass
-
-    return func.HttpResponse("tareas programadas", status_code=200)
+    msg.set(task_names)
+    return func.HttpResponse(f"tareas programadas: {task_names}", status_code=200)
 
 
 @framework.queue_trigger(
@@ -62,7 +77,7 @@ def execute_tasks_inqueue(msg: func.QueueMessage) -> None:
     task_name = msg.get_body().decode('utf-8')
     logger.info('execute_tasks_queue va ejecutar la tarea: %s', task_name)
     func_to_execute = runner.get_task(task_name)
-    emails = [""]
+    emails = runner.get_emails(task_name)
 
     if func_to_execute is None:
         raise TareaNoDefinida(task_name)
--- centraal_dataframework/runner.py
+++ centraal_dataframework/runner.py
@@ -1,8 +1,16 @@
 """Modulo de runner con las clases basicas para el framework."""
 import os
+import logging
 from typing import Callable, List
 
+import yaml
+
 from centraal_dataframework.excepciones import TareaDuplicada
+
+
+logger = logging.getLogger(__name__)
+OVEWRITE_TAREA = os.environ.get("SOBREESCRIBIR_TAREA", True)
+TAREA_DEBE_ESTAR_CONF = os.environ.get("TAREA_DEBE_CONFIGURACION", True)
 
 
 class Runner:
@@ -13,10 +21,38 @@ class Runner:
         self.tasks = {}
         self.logic_app_url = os.environ.get("logic_app_url", "")
         self._load_conf()
+        self._logic_app_url = self.conf["url_logicapp_email"]
+
+    @property
+    def logic_app_url(self) -> str:
+        """Obtiene Url logic app."""
+        return self._logic_app_url
 
     def add_task(self, func, task_name):
         """Adiciona tareas."""
-        if task_name not in self.tasks:
+        if TAREA_DEBE_ESTAR_CONF:
+            check_tarea_en_conf = task_name in self.conf["tareas"]
+        else:
+            check_tarea_en_conf = True
+
+        if OVEWRITE_TAREA and check_tarea_en_conf:
+            logging.warning(
+                "Tarea %s sobrescrita. Si quiere cambiar este comportamiento"
+                "use la variable de ambiente `SOBREESCRIBIR_TAREA` (valor actual = %s)",
+                task_name,
+                OVEWRITE_TAREA,
+            )
+            self.tasks[task_name] = func
+
+        elif not check_tarea_en_conf:
+            logger.error(
+                "La tarea %s debe estar definda en la configuracion. Tareas definidas: %s",
+                task_name,
+                self.conf["tareas"],
+            )
+            raise ValueError(f"{task_name} Debe estar definda en la configuración.")
+
+        elif task_name not in self.tasks:
             self.tasks[task_name] = func
         else:
             raise TareaDuplicada(task_name)
@@ -26,8 +62,23 @@ class Runner:
         task_fun = self.tasks.get(task_name)
         return task_fun
 
-    def _load_conf(task_name: str) -> List[str]:
-        return []
+    def es_programable(self, task_name_conf: dict, fecha) -> bool:
+        """verificar que la tarea puede ser programada."""
+        return True
 
-    def get_emails(task_name: str) -> List[str]:
-        return []
+    def get_tareas_programables(self, fecha) -> List[str]:
+        """Obtiene las tareas que pueden se programadas"""
+        return [task for task, task_conf in self.conf["tareas"] if self.es_programable(task_conf, fecha)]
+
+    def _load_conf(self) -> dict:
+        archivo = os.environ.get("YAML_CONFIGURACION", "centraal_dataframework.yaml")
+        logger.info("cargando configuracion desde %s", archivo)
+        with open(archivo, 'r', encoding="utf-8") as file:
+            data = yaml.safe_load(file)
+            self.conf = data
+
+    def get_emails(self, task_name: str = None) -> List[str]:
+        """Obtener los emails."""
+        if task_name is not None:
+            logger.warning("En esta version se soporta email generales, %s parametro ignorado", task_name)
+        return self.conf["emails_notificar"]
