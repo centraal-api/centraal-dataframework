aff11c097bbff11ea3c7ab2c31982e205f5483eb
German <german@centraal.studio>
2023-12-26 10:34:58 -0500

    Bump version: 0.1.2 → 0.1.3

--- centraal_dataframework/__init__.py
+++ centraal_dataframework/__init__.py
@@ -8,6 +8,6 @@ Modulos exportados por este paquete.
 
 __author__ = """centraal.studio"""
 __email__ = 'equipo@centraal.studio'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 from centraal_dataframework.tasks import task
